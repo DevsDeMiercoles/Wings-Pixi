a92d556f7236788ceef0f42eb87637d6a6060a3c
DevsDeMiercoles <DevsDeMiercoles@gmail.com>
2020-09-22 22:20:49 -0300

    sprite memory

---
+++ src/wings/framework/SpriteMemory.ts
@@ -0,0 +1,22 @@
+import { Container, DisplayObject, Renderer, RenderTexture, Sprite } from "pixi.js";
+
+export default class SpriteMemory extends Container {
+	private rt: RenderTexture;
+	private background: Sprite;
+	private renderer: Renderer;
+
+
+	constructor(renderer: Renderer) {
+		super();
+		this.renderer = renderer;
+
+		this.rt = RenderTexture.create({ width: renderer.view.width, height: renderer.view.height });
+		this.background = new Sprite(this.rt);
+		this.addChild(this.background);
+	}
+
+
+	remember(image: DisplayObject) {
+		this.renderer.render(image, this.rt, false);
+	}
+}
