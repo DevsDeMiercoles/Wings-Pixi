50df31c3f36f233ba95a5a593e7f7918c3ff2a51
DevsDeMiercoles <DevsDeMiercoles@gmail.com>
2020-09-23 14:41:07 -0300

    top down entity

---
+++ src/wings/engine/World/EntityTopDown.ts
@@ -0,0 +1,13 @@
+import FastMath from '../../framework/core/FastMath';
+import Entity from './Entity';
+
+export default abstract class EntityTopDown extends Entity {
+	protected lookFoward = true;
+
+	protected runPhysics(): void {
+		super.runPhysics();
+
+		if (this.speedLimit > 0) this.speed.limitMagnitude(this.speedLimit);
+		if (this.lookFoward) this.sprite.angle = this.speed.angle() * FastMath.toDegrees + 90;
+	}
+}
