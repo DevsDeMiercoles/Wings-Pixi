18a79f4d38cb2dd46b4f93dc2c19bf26c9224074
DevsDeMiercoles <DevsDeMiercoles@gmail.com>
2020-09-23 13:13:18 -0300

    update java transpiler

--- src/wings/framework/transpile/JavaToTypescript.js
+++ src/wings/framework/transpile/JavaToTypescript.js
@@ -1,4 +1,4 @@
-filename = "code.java";
+filename = "InputProxy.java";
 
 fs = require('fs');
 fs.readFile(filename, 'utf8', function (err, data) {
@@ -8,15 +8,17 @@ fs.readFile(filename, 'utf8', function (err, data) {
 
 function processData(javaCode) {
 	javaCode = javaCode.replace(/final /g, "")
-		.replace(/public class/g, "export default class")
+		.replace(/public( abstract)* class/g, "export default class")
 		.replace(/ObjectMap/g, "Map")
-		.replace(/^\t(public|private) (.*<.*?>) ([a-z1-9A-Z ]+)/gm, "\t$1 $3:$2")
+		.replace(/^\t(public|private) ([a-z1-9A-Z ]+)(<.*?>)* ([a-z1-9A-Z ]+)(;|=)/gm, "\t$1 $4:$2$3$5")
 		.replace(/^\t(public|private) ([a-z1-9A-Z ]+) (.*) {$/gm, "\t$1 $3:$2 {")
 		.replace(/([a-z1-9A-Z]+) ([a-z1-9A-Z]+),/gm, "$2:$1,")
 		.replace(/([a-z1-9A-Z]+) ([a-z1-9A-Z]+)\)/gm, "$2:$1)")
 		.replace(/(\t+)public/g, "$1")
 		.replace(/String/g, "string")
-		.replace(/(float|int)/g, "number");
+		.replace(/(float|int)/g, "number")
+		.replace(/(Float|Integer)/g, "Number")
+		.replace(/containsKey/g, "has");
 
 
 	fs.writeFile('exported ts.testing', javaCode, function (err) {
