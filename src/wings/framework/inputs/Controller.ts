71579366447000ea5eb0d97292688cc0c4f852da
DevsDeMiercoles <DevsDeMiercoles@gmail.com>
2020-09-24 08:03:24 -0300

    created mouse controller

--- src/wings/framework/inputs/Controller.ts
+++ src/wings/framework/inputs/Controller.ts
@@ -1,5 +1,7 @@
-import LinkedList from "../core/collections/LinkedList";
+import { DisplayObject } from "pixi.js";
+import Position from '../physics/Position';
 import { Keyboard, KeysCode } from './Keyboard';
+import { Mouse, MouseKeys } from "./Mouse";
 
 /* Enums */
 export enum InputEvents {
@@ -12,13 +14,12 @@ export default class Controller {
 	private actionsMap = new Map<string, Map<InputEvents, string>>();
 	private actionsQueue = new Array<string>();
 	private keyboard: Keyboard;
+	private mouse: Mouse | undefined;
 
+	constructor(interaction?: DisplayObject) {
 		this.keyboard = new Keyboard(this.newKeyAction.bind(this));
-	private pressedKeys = new LinkedList<string>();
-
-	constructor() {
-		window.addEventListener("keydown", this.onKeyDown.bind(this));
-		window.addEventListener("keyup", this.onKeyUp.bind(this));
+		if (interaction)
+			this.mouse = new Mouse(interaction, this.newKeyAction.bind(this));
 	}
 
 	private newKeyAction(type: InputEvents, key: string) {
@@ -37,12 +38,17 @@ export default class Controller {
 			if (this.actionsMap.get(key)?.has(InputEvents.KeyIsDown))
 				actionsOcurred.push(this.actionsMap.get(key)?.get(InputEvents.KeyIsDown)!);
 		}
+		if (this.mouse?.isButtonDown(MouseKeys.LEFT) && this.actionsMap.get(MouseKeys.LEFT)?.has(InputEvents.KeyIsDown))
+			actionsOcurred.push(this.actionsMap.get(MouseKeys.LEFT)?.get(InputEvents.KeyIsDown)!);
+		if (this.mouse?.isButtonDown(MouseKeys.RIGHT) && this.actionsMap.get(MouseKeys.RIGHT)?.has(InputEvents.KeyIsDown))
+			actionsOcurred.push(this.actionsMap.get(MouseKeys.RIGHT)?.get(InputEvents.KeyIsDown)!);
+
 		this.actionsQueue.length = 0;
 
 		return actionsOcurred;
 	}
 
-	addAction(action: string, key: KeysCode, event: InputEvents): void {
+	addAction(action: string, key: KeysCode | MouseKeys, event: InputEvents): void {
 		if (!this.actionsMap.has(key))
 			this.actionsMap.set(key, new Map());
 
@@ -62,121 +68,15 @@ export default class Controller {
 		}
 	}
 
-	isKeyDown(key: KeysCode): boolean {
-		return this.pressedKeys.includes(key);
+	isKeyDown(key: KeysCode | MouseKeys): boolean {
+		if (key == MouseKeys.LEFT || key == MouseKeys.RIGHT)
+			return this.mouse?.isButtonDown(key) ?? false;
+		else
+			return this.keyboard.isKeyDown(key);
+
 	}
-}
-
-export enum KeysCode {
-	ANY_KEY = "Why not all?",
-	UNKNOWN = "I dunno",
-
-	NUM_0 = "Digit0",
-	NUM_1 = "Digit1",
-	NUM_2 = "Digit2",
-	NUM_3 = "Digit3",
-	NUM_4 = "Digit4",
-	NUM_5 = "Digit5",
-	NUM_6 = "Digit6",
-	NUM_7 = "Digit7",
-	NUM_8 = "Digit8",
-	NUM_9 = "Digit9",
-
-	A = "KeyA",
-	B = "KeyB",
-	C = "KeyC",
-	D = "KeyD",
-	E = "KeyE",
-	F = "KeyF",
-	G = "KeyG",
-	H = "KeyH",
-	I = "KeyI",
-	J = "KeyJ",
-	K = "KeyK",
-	L = "KeyL",
-	M = "KeyM",
-	N = "KeyN",
-	O = "KeyO",
-	P = "KeyP",
-	Q = "KeyQ",
-	R = "KeyR",
-	S = "KeyS",
-	T = "KeyT",
-	U = "KeyU",
-	V = "KeyV",
-	W = "KeyW",
-	X = "KeyX",
-	Y = "KeyY",
-	Z = "KeyZ",
-
-	F1 = "F1",
-	F2 = "F2",
-	F3 = "F3",
-	F4 = "F4",
-	F5 = "F5",
-	F6 = "F6",
-	F7 = "F7",
-	F8 = "F8",
-	F9 = "F9",
-	F10 = "F10",
-	F11 = "F11",
-	F12 = "F12",
-
-	BACK_QUOTE = "Backquote",
-	BACK_SLASH = "IntlBackslash",
-	MINUS = "Minus",
-	EQUALS = "Equals",
-	BRACKET_OPEN = "BracketLeft",
-	BRACKET_CLOSE = "BracketRight",
-	QUOTE = "Quote",
-	BACKLASH = "Backslash",
-	COMMA = "Comma",
-	PERIOD = "Period",
-	SLASH = "Slash",
-
-	TAB = "Tab",
-	SPACE = "Space",
-	ENTER = "Enter",
-	ESCAPE = "Escape",
-	BACKSPACE = "Backspace",
-
-	ALT_LEFT = "AltLeft",
-	ALT_RIGHT = "AltRight",
-	CONTROL_LEFT = "ControlLeft",
-	CONTROL_RIGHT = "ControlRight",
-	SHIFT_LEFT = "ShiftRight",
-	SHIFT_RIGHT = "ShiftRight",
-	CONTEXT_MENU = "ContextMenu",
-	META_LEFT = "MetaLeft",
-
-	INSERT = "Insert",
-	DEL = "Delete",
-	HOME = "Home",
-	END = "End",
-	PAGE_UP = "PageUp",
-	PAGE_DOWN = "PageDown",
-
-	NUM = "NumLock",
-	NUM_DIVIDE = "NumpadDivide",
-	NUM_MULTIPLY = "NumpadMultiply",
-	NUM_SUBSTRACT = "NumpadSubtract",
-	NUM_ADD = "NumpadAdd",
-	NUM_ENTER = "NumpadEnter",
-	NUM_DECIMAL = "NumpadDecimal",
-	NUMPAD_0 = "Numpad0",
-	NUMPAD_1 = "Numpad1",
-	NUMPAD_2 = "Numpad2",
-	NUMPAD_3 = "Numpad3",
-	NUMPAD_4 = "Numpad4",
-	NUMPAD_5 = "Numpad5",
-	NUMPAD_6 = "Numpad6",
-	NUMPAD_7 = "Numpad7",
-	NUMPAD_8 = "Numpad8",
-	NUMPAD_9 = "Numpad9",
-
-	DOWN = "ArrowDown",
-	LEFT = "ArrowLeft",
-	RIGHT = "ArrowRight",
-	UP = "ArrowUp",
+	get mousePos(): Position {
+		return this.mouse?.position ?? new Position();
+	}
 }
 
--- src/wings/framework/inputs/Mouse.ts
+++ src/wings/framework/inputs/Mouse.ts
@@ -1,3 +1,36 @@
+import { DisplayObject } from "pixi.js";
+import Position from '../physics/Position';
+import { ControllerCallback, InputEvents } from './Controller';
+
+export class Mouse {
+	private callback: ControllerCallback;
+	private leftDown = false;
+	private rightDown = false;
+	position = new Position();
+
+	constructor(interactive: DisplayObject, callback: ControllerCallback) {
+		this.callback = callback;
+
+		interactive.interactive = true;
+		interactive.on(MouseEvents.MOUSE_DOWN, () => { this.callback(InputEvents.KeyIsDown, MouseKeys.LEFT); this.leftDown = true; });
+		interactive.on(MouseEvents.MOUSE_UP, () => { this.callback(InputEvents.KeyUp, MouseKeys.LEFT); this.leftDown = false; });
+		interactive.on(MouseEvents.CLICK, () => { this.callback(InputEvents.Click, MouseKeys.LEFT); });
+		interactive.on(MouseEvents.RIGHT_DOWN, () => { this.callback(InputEvents.KeyIsDown, MouseKeys.RIGHT); this.rightDown = true; });
+		interactive.on(MouseEvents.RIGHT_UP, () => { this.callback(InputEvents.KeyUp, MouseKeys.RIGHT); this.rightDown = false; });
+		interactive.on(MouseEvents.RIGHT_CLICK, () => { this.callback(InputEvents.Click, MouseKeys.RIGHT); });
+		interactive.on(MouseEvents.POINTER_MOVE, (e) => { this.position.copyFrom(e.data.global); });
+	}
+	isButtonDown(type: MouseKeys) {
+		if (type == MouseKeys.LEFT)
+			return this.leftDown;
+		else
+			return this.rightDown;
+	}
+}
+export enum MouseKeys {
+	LEFT = "left_button",
+	RIGHT = "right_button",
+}
 export enum MouseEvents {
 	CLICK = "click",
 	MOUSE_DOWN = "mousedown",
