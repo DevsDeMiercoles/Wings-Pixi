3afbc47ece902d9baa7df9cf43a04de3ce594dbd
DevsDeMiercoles <DevsDeMiercoles@gmail.com>
2020-09-25 09:53:23 -0300

    short term memory for sprites

--- src/wings/framework/SpriteMemory.ts
+++ src/wings/framework/SpriteMemory.ts
@@ -2,6 +2,7 @@ import { Container, DisplayObject, Renderer, RenderTexture, Sprite } from "pixi.
 
 export default class SpriteMemory extends Container {
 	private rt: RenderTexture;
+	private rt2: RenderTexture;
 	private background: Sprite;
 	private renderer: Renderer;
 
@@ -11,6 +12,7 @@ export default class SpriteMemory extends Container {
 		this.renderer = renderer;
 
 		this.rt = RenderTexture.create({ width: renderer.view.width, height: renderer.view.height, resolution: 2 });
+		this.rt2 = RenderTexture.create({ width: renderer.view.width, height: renderer.view.height, resolution: 2 });
 		this.background = new Sprite(this.rt);
 
 		this.addChild(this.background);
@@ -19,5 +21,23 @@ export default class SpriteMemory extends Container {
 
 	remember(image: DisplayObject) {
 		this.renderer.render(image, this.rt, false);
+	}
+	shortTermMemory(sprite: Container, fade = 0.5) {
+		let alpha = this.background.alpha;
+		this.background.alpha = fade;
+
+		this.renderer.render(this.background, this.rt2);
+		this.renderer;
+
+		this.background.alpha = alpha;
+
+		this.exchangeTextures();
+		this.remember(sprite);
+	}
+	private exchangeTextures() {
+		let temp = this.rt;
+		this.rt = this.rt2;
+		this.rt2 = temp;
+		this.background.texture = this.rt;
 	}
 }
